38d539124c8daea6c52b225e68d9a2bc31484436
Diego Antonio Cayllahua Muñoz <ext.dcayllahuam.cleveritgroup+psalud@pacificosegurossalud.com.pe>
2026-01-12 05:44:05 -0500

    feat: added test integration

--- config/database.php
+++ config/database.php
@@ -2,15 +2,17 @@
 
 declare(strict_types=1);
 
+
 return [
     'mysql' => [
         'dsn' => sprintf(
-            'mysql:host=%s;dbname=%s;charset=utf8mb4',
-            $_ENV['DB_HOST'],
-            $_ENV['DB_NAME']
+            'mysql:host=%s;port=%s;dbname=%s;charset=utf8mb4',
+            getenv('DB_HOST'),
+            getenv('DB_PORT'),
+            getenv('DB_NAME'),
         ),
-        'user' => $_ENV['DB_USER'],
-        'password' => $_ENV['DB_PASSWORD'],
-        'port' => $_ENV['DB_PORT']
+        'user' => getenv('DB_USER'),
+        'password' => getenv('DB_PASSWORD'),
+        'port' => getenv('DB_PORT')
     ],
 ];
