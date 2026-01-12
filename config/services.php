b6dcc4f9be48c2ed5c5d7e47f9f58a90ff05a889
Diego Antonio Cayllahua Muñoz <ext.dcayllahuam.cleveritgroup+psalud@pacificosegurossalud.com.pe>
2026-01-12 11:10:35 -0500

    feat: update test integraton

--- config/services.php
+++ config/services.php
@@ -2,12 +2,12 @@
 declare(strict_types=1);
 return [
     'payment' => [
-        'base_uri' => $_ENV['PAYMENT_BASE_URI'],
+        'base_uri' => getenv('PAYMENT_BASE_URI'),
         'timeout' => 3,
         'retries' => 3,
     ],
     'notification' => [
-        'base_uri' => $_ENV['NOTIFICATION_BASE_URI'],
+        'base_uri' => getenv('NOTIFICATION_BASE_URI'),
         'timeout' => 2,
     ],
 ];
--- tests/Integration/UsesCases/ReservationUseCaseTest.php
+++ tests/Integration/UsesCases/ReservationUseCaseTest.php
@@ -15,42 +15,51 @@ final class ReservationUseCaseTest extends TestCase
 
     public static function setUpBeforeClass(): void
     {
+        $mysqlName = 'mysql_test_' . uniqid();
 
         self::$mysql = (new MySQLContainer())
             ->withMySQLDatabase('app_test')
             ->withMySQLUser('app','secret')
-            ->withName('mysql_test_' . uniqid())
+            ->withName($mysqlName)
+            ->withNetwork('test_network')
+            ->withHealthCheckCommand(
+                'mysqladmin ping -h localhost -u app -psecret',
+                1000,
+                3000,
+                10,
+                0
+            )
             ->start();
-
         $mysqlHost = self::$mysql->getHost();
-        $mysqlPort = self::$mysql->getMappedPort(3306);
-        putenv("DB_HOST=$mysqlHost");
-        putenv("DB_PORT=$mysqlPort");
-        putenv("DB_NAME=app_test");
-        putenv("DB_USER=app");
-        putenv("DB_PASSWORD=secret");
-        self::$appContainer = (new GenericContainer('reservations_land_gorilla-app'))
-            ->withExposedPorts(80)
-            ->withEnvironment([
-                'DB_HOST' => $mysqlHost,
-                'DB_PORT' => $mysqlPort,
-                'DB_NAME' => 'app_test',
-                'DB_USER' => 'app',
-                'DB_PASSWORD' => 'secret',
-                'PAYMENT_BASE_URI' => 'https://55b73bf4-c233-4ab2-b2fa-b8a67d60e2c8.mock.pstmn.io',
-                'NOTIFICATION_BASE_URI' => 'https://55b73bf4-c233-4ab2-b2fa-b8a67d60e2c8.mock.pstmn.io'
-            ])
-            ->withName('app_test_' . uniqid())
-            ->start();
-        $mappedPort = self::$appContainer->getMappedPort(80);
-        $host = self::$appContainer->getHost();
-        self::$baseUri = "http://$host:$mappedPort";
+        $mysqlPort =  self::$mysql->getMappedPort(3306);
+
         $envString = sprintf(
             'DB_HOST=%s DB_PORT=%s DB_NAME=%s DB_USER=%s DB_PASSWORD=%s',
             $mysqlHost, $mysqlPort, 'app_test', 'app', 'secret'
         );
         exec("$envString vendor/bin/phinx migrate -c db/phinx.php", $output1, $return1);
         exec("$envString vendor/bin/phinx seed:run -c db/phinx.php", $output2, $return2);
+        echo "Phinx migrate return: $return1\nOutput:\n"; print_r($output1);
+        echo "Phinx seed return: $return2\nOutput:\n"; print_r($output2);
+
+        self::$appContainer = (new GenericContainer('reservations_land_gorilla-app'))
+            ->withExposedPorts(80)
+            ->withEnvironment([
+                'DB_HOST' => $mysqlName,
+                'DB_PORT' => 3306,
+                'DB_NAME' => 'app_test',
+                'DB_USER' => 'app',
+                'DB_PASSWORD' => 'secret',
+                'PAYMENT_BASE_URI' => 'https://55b73bf4-c233-4ab2-b2fa-b8a67d60e2c8.mock.pstmn.io',
+                'NOTIFICATION_BASE_URI' => 'https://55b73bf4-c233-4ab2-b2fa-b8a67d60e2c8.mock.pstmn.io'
+            ])
+            ->withNetwork('test_network')
+            ->withName('app_test_' . uniqid())
+            ->start();
+        $mappedPort = self::$appContainer->getMappedPort(80);
+        $host = self::$appContainer->getHost();
+        self::$baseUri = "http://$host:$mappedPort";
+
     }
 
     public static function tearDownAfterClass(): void
@@ -73,7 +82,6 @@ final class ReservationUseCaseTest extends TestCase
             "headers" => ['Idempotency-Key' => uniqid('key_', true)]
         ]);
         $reservationPending =json_decode($client->get('/reservations/1')->getBody()->getContents(), true);
-        echo $client->get('/reservations/1')->getBody()->getContents();
         $statusPending = $reservationPending["status"];
         $this->assertSame('pending', $statusPending);
         $this->assertSame(201, $response->getStatusCode());
@@ -82,10 +90,21 @@ final class ReservationUseCaseTest extends TestCase
         $client->post('/reservations/1/confirm');
         $reservationNeedsConfirm = json_decode($client->get('/reservations/1')->getBody()->getContents(), true);
         $statusNeedsConfirm = $reservationNeedsConfirm["status"];
-        $this->assertSame('needs_confirm', $statusNeedsConfirm);
-
-
-        exec("bin/process-outbox");
+        $this->assertSame('needs_confirmation', $statusNeedsConfirm);
+        $mysqlHost = self::$mysql->getHost();
+        $mysqlPort =  self::$mysql->getMappedPort(3306);
+        $envString = sprintf(
+            'DB_HOST=%s DB_PORT=%s DB_NAME=%s DB_USER=%s DB_PASSWORD=%s PAYMENT_BASE_URI=%s  NOTIFICATION_BASE_URI=%s ',
+            $mysqlHost,
+            $mysqlPort,
+            'app_test',
+            'app',
+            'secret','
+            https://55b73bf4-c233-4ab2-b2fa-b8a67d60e2c8.mock.pstmn.io',
+            'https://55b73bf4-c233-4ab2-b2fa-b8a67d60e2c8.mock.pstmn.io'
+        );
+        exec("$envString php bin/process-outbox.php",$output, $returnVar);
+        echo "Process-outbox return code: $returnVar\nOutput:\n"; print_r($output);
 
         $reservationConfirmed = json_decode($client->get('/reservations/1')->getBody()->getContents(), true);
         $statusConfirmed = $reservationConfirmed["status"];
